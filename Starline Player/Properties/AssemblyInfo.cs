09f9eef17d416753f71b4d0a3554bdd11a17aaa4
Darksempaii <cesar_enoc666@hotmail.com>
2021-01-06 18:15:29 -0600

    10

--- Starline Player/Properties/AssemblyInfo.cs
+++ Starline Player/Properties/AssemblyInfo.cs
@@ -1,26 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Startline")]
+[assembly: AssemblyTitle("Starline Player")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dark Company")]
-[assembly: AssemblyProduct("Start Line")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Starline Player")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("Start Line Project")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
 // para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
 // COM, establezca el atributo ComVisible en true en este tipo.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("69235492-af5d-46f2-a434-c62e9d7f01ec")]
+[assembly: Guid("b7a00206-ec52-4879-9087-d0b2d39c48c5")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
@@ -32,6 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.2.9")]
-[assembly: AssemblyFileVersion("0.6.2.9")]
-[assembly: NeutralResourcesLanguage("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
